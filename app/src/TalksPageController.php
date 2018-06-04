ea45e15cf586141536db4a02a73ba3cdb2ffa198
montyD <hello@montydawson.co.uk>
2018-06-04 21:44:11 +0100

    Setting up talks controller

--- app/src/TalksPageController.php
+++ app/src/TalksPageController.php
@@ -5,11 +5,10 @@ namespace HSC;
 use PageController;
 
 class TalksPageController extends PageController {
-
     private static $allowed_actions = [];
 
     public function init() {
       parent::init();
-      Requirements::javascript('https://maps.googleapis.com/maps/api/js?key=AIzaSyDeK6LmQ6RljdxyUWMnzfob-dsqIrrxHZ4');
+      Requirements::themedJavascript('talks');
     }
 }
